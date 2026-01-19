4c04114160e4c0d2957bcb6b39e092b72c4b98b9
Ben Henderson <benhenderson976@gmail.com>
2026-01-19 21:45:37 +0000

    FEAT (ui) replaced depricated object key

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['cdn2.thecatapi.com'],
+    remotePatterns: [{ hostname: 'cdn2.thecatapi.com' }],
     formats: ['image/avif', 'image/webp'],
   },
 };
